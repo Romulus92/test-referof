e73734084a1cdbddf7b6b60c993e9cca7013d3a2
Роман Зайнуллин <r.zaynullin@tapir.ws>
2025-08-04 21:19:26 +0500

    WIP basic setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Modal from './components/Modal.vue'
+</script>
+
+<template>
+  <Modal/>
+</template>
+
+<style>
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,8 @@
+<template>
+	<button class="button modal-button">
+		Создать промокод
+	</button>
+	<div class="modal">
+		Модалка
+	</div>
+</template>
